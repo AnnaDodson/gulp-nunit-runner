81fe8bcdf5925325c1f7d8dedf20d7bf7f4100c6
Anna Dodson <anna.dodson@imosphere.co.uk>
2018-11-28 15:41:50 +0000

    Fixed known NPM security vulnerabilities
    - Ran npm audit and fixed the vulnerabilities
    - Update to Gulp required refactor of how tasks are called

--- gulpfile.js
+++ gulpfile.js
@@ -4,20 +4,24 @@ var gulp = require('gulp'),
 	filter = require('gulp-filter'),
 	mocha = require('gulp-mocha');
 
-gulp.task('default', ['test']);
 
-gulp.task('watch', function () {
-	gulp.watch(['**/*.js', '!node_modules/**/*.js'], ['test']);
-});
+function watch(){
+	gulp.watch(['**/*.js', '!node_modules/**/*.js'], test);
+}
 
-gulp.task('test', ['lint'], function () {
-	gulp.src('test/*.js', {read: false})
+function test(){
+	return gulp.src('test/*.js', {read: false})
 		.pipe(mocha({reporter: 'spec', ui: 'bdd'}));
-});
+}
 
-gulp.task('lint', function () {
+function lint(){
 	return gulp.src('**/*.js')
 		.pipe(filter(['*', '!node_modules/**/*']))
 		.pipe(jshint({node: true}))
 		.pipe(jshint.reporter('default'));
-});
+}
+
+var runTests = gulp.series(lint, test);
+
+gulp.task('default', runTests);
+gulp.task('watch', watch);
